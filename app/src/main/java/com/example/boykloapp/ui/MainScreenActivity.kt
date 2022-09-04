3433aba2e6bb08367fc0fb85be5f95d7dcb90a05
Alparslan Sariaydin <sariaydinalparslan@gmail.com>
2022-09-04 15:38:30 +0300

    crash hali

---
+++ app/src/main/java/com/example/boykloapp/ui/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.boykloapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foxycode.bedenolcer.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
