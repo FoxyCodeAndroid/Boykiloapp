16a9d5ffd6ce8e52247d910ab74d170c00002642
Alparslan Sariaydin <sariaydinalparslan@gmail.com>
2022-10-07 03:24:05 +0300

    fixed ui problems

--- app/src/main/java/com/example/boykloapp/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/boykloapp/ui/SettingsFragment.kt
@@ -1,20 +1,16 @@
 package com.example.boykloapp.ui
 
+import android.R
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.example.boykloapp.utils.SettingsUtils
-import com.foxycode.bedenolcer.R
-import kotlinx.android.synthetic.main.activity_main_screen.*
-import kotlinx.android.synthetic.main.fragment_single.*
+
 
 class SettingsFragment : PreferenceFragmentCompat(),SharedPreferences.OnSharedPreferenceChangeListener {
 
@@ -24,14 +20,15 @@ class SettingsFragment : PreferenceFragmentCompat(),SharedPreferences.OnSharedPr
             .registerOnSharedPreferenceChangeListener(this)
     }
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        setPreferencesFromResource(com.foxycode.bedenolcer.R.xml.root_preferences, rootKey)
 
     }
     @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.main_gray2))
+        view.setBackgroundColor(ContextCompat.getColor(requireContext(), com.foxycode.bedenolcer.R.color.main_gray2))
     }
+
     override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
        if (p1=="mass_type"){
            val prefs = p0?.getString(p1,"1")
@@ -51,4 +48,5 @@ class SettingsFragment : PreferenceFragmentCompat(),SharedPreferences.OnSharedPr
            }
        }
     }
+
 }
